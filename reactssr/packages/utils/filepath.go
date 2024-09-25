e8bf5d06817a7c68824467832aba84ac7833c1b9
Ravi Lamichhane <ravilamichhane30@gmail.com>
2024-09-25 17:07:22 +0545

    remove reactssr

--- reactssr/packages/utils/filepath.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-import "path/filepath"
-
-// Returns the absolute path of the file without returning an error
-func GetFullFilePath(filePath string) string {
-	fp, _ := filepath.Abs(filePath)
-	return filepath.ToSlash(fp)
-}
