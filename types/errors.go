56ef1fa1ffaf81b177f918932f1b1f9ceea2d8c2
Ravi Lamichhane <ravilamichhane30@gmail.com>
2024-10-19 21:21:05 +0545

    cleanup

--- types/errors.go
+++
@@ -1,9 +0,0 @@
-package types
-
-import (
-	"github.com/golang-jwt/jwt/v5"
-	"gorm.io/gorm"
-)
-
-var ErrTokenExpired = jwt.ErrTokenExpired
-var ErrRecordNotFound = gorm.ErrRecordNotFound
--- web/response.go
+++
@@ -1,9 +0,0 @@
-package web
-
-type SuccessMessage struct {
-	Message string `json:"message"`
-}
-
-func NewSuccessMessage(message string) SuccessMessage {
-	return SuccessMessage{Message: message}
-}
